9aad2fb5c544484c22e35afd3eaf88fa08fb48cf
Korvin <korvin09071989@gmail.com>
2020-03-19 15:26:22 +0200

    Попытка чего то

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Кнопка мобильного меню сворачивания
+document.getElementById('button_mobile__menu').onclick = function () {
+    document.getElementById('button_mobile__menu').classList.toggle('active');
+};
+
+
+let user = {
+    name: 'News',
+    author: 'Korvin',
+    date: '19.03.2020',
+    topic: 'Education',
+};
+
+console.log (user);
+
+
+
+
+
+
+
+
